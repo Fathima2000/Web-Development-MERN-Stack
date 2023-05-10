337453d6300c887493341adcde90930e4393dcd7
Fathima2000 <sombolafatty@gmail.com>
2023-05-10 08:53:11 +0530

    feat: lifting state up

---
+++ 6. React/Additional/my-app/src/App.js
@@ -0,0 +1,11 @@
+import DifferentState from "./Components/DifferentState";
+import SharedState from "./Components/SharedState";
+
+export default function App(){
+  return (
+    <div>
+        {/* <DifferentState/> */}
+        <SharedState/>
+    </div>
+  )
+}
